63d2538807ce36feec71b2f8892bb5f0f433f945
Vinicius Gaio <vinagamer9@gmail.com>
2022-06-29 16:48:30 -0300

    Update(265): Foreing key not null

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -1,23 +1,41 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System;
 
 namespace SalesWebMvc.Models
 {
     public class Seller
     {
-       
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public DateTime BirthDate { get; set; }
-        public double BaseSalary { get; set; }
-        public Department Department { get; set; }
-        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-   
-        public Seller(){}
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department) 
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public double BaseSalary { get; set; }
+
+        public Department Department { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public ICollection<SalesRecord> Sales
+        { get; set;
+        } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(
+            int id,
+            string name,
+            string email,
+            DateTime birthDate,
+            double baseSalary,
+            Department department
+        )
         {
             this.Id = id;
             this.Name = name;
@@ -29,16 +47,19 @@ namespace SalesWebMvc.Models
 
         public void AddSales(SalesRecord sr)
         {
-            Sales.Add(sr);
+            Sales.Add (sr);
         }
+
         public void RemoveSales(SalesRecord sr)
         {
-            Sales.Remove(sr);
+            Sales.Remove (sr);
         }
+
         public double TotalSales(DateTime initial, DateTime final)
         {
-            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+            return Sales
+                .Where(sr => sr.Date >= initial && sr.Date <= final)
+                .Sum(sr => sr.Amount);
         }
-   
     }
 }
