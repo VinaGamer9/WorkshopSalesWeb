b56544b0be5f6b9172d287c3806b3b023b40ac67
Vinicius Gaio <vinagamer9@gmail.com>
2022-07-01 11:35:54 -0300

    Update(272): Validation

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -45,6 +45,16 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel =
+                    new SellerFormViewModel {
+                        Seller = seller,
+                        Departments = departments
+                    };
+                return View(viewModel);
+            }
             _sellerService.Insert (seller);
             return RedirectToAction(nameof(Index));
         }
@@ -120,6 +130,16 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel =
+                    new SellerFormViewModel {
+                        Seller = seller,
+                        Departments = departments
+                    };
+                return View(viewModel);
+            }
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error),
