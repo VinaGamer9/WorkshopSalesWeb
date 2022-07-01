3b1213e53df13d7ee0dd79de6b9de518c313d0da
Vinicius Gaio <vinagamer9@gmail.com>
2022-07-01 13:44:02 -0300

    Update(275): Preparing sales search navigation views

---
+++ Controllers/SalesRecordsController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        private readonly ILogger<SalesRecordsController> _logger;
+
+        public SalesRecordsController(ILogger<SalesRecordsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [
+            ResponseCache(
+                Duration = 0,
+                Location = ResponseCacheLocation.None,
+                NoStore = true)
+        ]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
