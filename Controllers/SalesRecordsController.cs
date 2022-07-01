120359719fc77fc5d63804706839e2c5ae59749f
Vinicius Gaio <vinagamer9@gmail.com>
2022-07-01 14:11:05 -0300

    Update(276): Implementing simple search

--- Controllers/SalesRecordsController.cs
+++ Controllers/SalesRecordsController.cs
@@ -5,16 +5,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
-        private readonly ILogger<SalesRecordsController> _logger;
+        private readonly SalesRecordService _salesRecordService;
 
-        public SalesRecordsController(ILogger<SalesRecordsController> logger)
+        public SalesRecordsController(SalesRecordService salesRecordService)
         {
-            _logger = logger;
+            this._salesRecordService = salesRecordService;
         }
 
         public IActionResult Index()
@@ -22,20 +23,22 @@ namespace SalesWebMvc.Controllers
             return View();
         }
 
-        [
-            ResponseCache(
-                Duration = 0,
-                Location = ResponseCacheLocation.None,
-                NoStore = true)
-        ]
-        public IActionResult Error()
+        public async Task<IActionResult>
+        SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View("Error!");
-        }
-
-        public IActionResult SimpleSearch()
-        {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result =
+                await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
         public IActionResult GroupingSearch()
